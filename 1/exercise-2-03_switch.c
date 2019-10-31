7cf0e955b98efbb56e14abc729eb7b06baf2917f
Felix <x3mdroid@gmail.com>
2019-10-31 19:08:52 +0100

    switch implementierung

--- 1/exercise-2-03_switch.c
+++ 1/exercise-2-03_switch.c
@@ -6,44 +6,36 @@ int main(int argc, char *argv[])
   int val = (argc > 1) ? atoi(argv[1]) : 0;
 
   // Erweitern Sie fuer die Zahlen 0-9, Bsp. für 0
-  if (val == 0)
-  {
-      printf("null");
-  }
-  if (val == 1)
-  {
-      printf("eins");
-  }
-  if (val == 2)
-  {
-      printf("zwei");
-  }
-  if (val == 3)
-  {
-      printf("drei");
-  }
-  if (val == 4)
-  {
-      printf("vier");
-  }
-  if (val == 5)
-  {
-      printf("fünf");
-  }
-  if (val == 6)
-  {
-      printf("sechs");
-  }
-  if (val == 7)
-  {
-      printf("sieben");
-  }
-  if (val == 8)
-  {
-      printf("acht");
-  }
- if (val == 9)
-  {
-      printf("neun");
-  }
+switch (val) {
+case 0: 
+	printf("null");
+	break;
+case 1:
+	printf("eins");
+	break;
+case 2:
+	printf("zwei");
+	break;
+case 3:
+	printf("drei");
+	break;
+case 4:
+	printf("vier");
+	break;
+case 5:
+	printf("fuenf");
+	break;
+case 6:
+	printf("sechs");
+	break;
+case 7:
+	printf("sieben");
+	break;
+case 8:
+	printf("acht");
+	break;
+case 9:
+	printf("neun");
+	break;
+}
 }
