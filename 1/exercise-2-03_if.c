058f9c8840f1a58933adc32e42ef7ac675522a9d
Steffen <steff.schubert@gmx.de>
2019-10-31 18:54:58 +0100

    Aufg 3, programm mit if geadded

---
+++ 1/exercise-2-03_if.c
@@ -0,0 +1,49 @@
+#include <stdio.h>      /* printf */
+#include <stdlib.h>     /* atoi */
+
+int main(int argc, char *argv[])
+{
+  int val = (argc > 1) ? atoi(argv[1]) : 0;
+
+  // Erweitern Sie fuer die Zahlen 0-9, Bsp. für 0
+  if (val == 0)
+  {
+      printf("null");
+  }
+  if (val == 1)
+  {
+      printf("eins");
+  }
+  if (val == 2)
+  {
+      printf("zwei");
+  }
+  if (val == 3)
+  {
+      printf("drei");
+  }
+  if (val == 4)
+  {
+      printf("vier");
+  }
+  if (val == 5)
+  {
+      printf("fünf");
+  }
+  if (val == 6)
+  {
+      printf("sechs");
+  }
+  if (val == 7)
+  {
+      printf("sieben");
+  }
+  if (val == 8)
+  {
+      printf("acht");
+  }
+ if (val == 9)
+  {
+      printf("neun");
+  }
+}
