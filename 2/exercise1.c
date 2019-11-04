b111690bb1d6dae2f6c8629196c6a2493929f013
Felix <x3mdroid@gmail.com>
2019-11-04 16:37:31 +0100

    aufgabe 2&3 bearbeitet

--- 2/exercise1.c
+++ 2/exercise1.c
@@ -1,16 +1,30 @@
 #include <stdio.h>
 
-struct foo
+//toplevel struct mit einer union
+struct foobar
 {
-	int a;
-	int b;
-};
+	enum	{
+		INT,
+		FLOAT
+	} type ;
+	
+	//union aus zwei structs zur Speichersparung
+	union barfoo
+	{
+		struct foo
+		{
+			int a;
+			int b;
+		}foo;
+		
+		struct bar
+		{
+			float a;
+			float b;
+		}bar;
+	}barfoo;
+}foobar;
 
-struct bar
-{
-	float a;
-	float b;
-};
 
 int main (void)
 {
@@ -18,5 +32,6 @@ int main (void)
 	printf("foo: %d %d\n", a.a, a.b);
 	struct bar b = { 23.0, 42.0 };
 	printf("bar: %f %f\n", b.a, b.b);
+	printf("sizeof(foobar): %ld\n", sizeof(foobar));
 	return 0;
 }
