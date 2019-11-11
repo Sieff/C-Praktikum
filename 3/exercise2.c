7133637303419aca71213f6955217a56ec902297
Felix <x3mdroid@gmail.com>
2019-11-11 20:55:31 +0100

    Aufgabe 2 bugfix

--- 3/exercise2.c
+++ 3/exercise2.c
@@ -1,21 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int * extendByOne(int array[], int arraySize)
+int* extendByOne(int array[], int arraySize)
 {	
-	int *ret = (int *) malloc(sizeof(int *) * (arraySize + 1));
-	printf("%d\n", array[0]);
-	ret = array;
-	printf("%d\n", ret[0]);
+	int *ret;
+	ret = malloc(sizeof(int) * (arraySize + 1));
+	if (array == NULL)
+	{
+		printf("can not allocate memory.");
+		exit(0);
+	}
+	//ret = array; //CAUSES ERROR, functionality removed
+	for (int i = 0; i < arraySize; i++)
+	{
+		*(ret + i) = array[i];
+		//printf("%d\n", *(ret+i));
+	}
 	ret[arraySize] = 0;
 	return ret;
 }
 
-int main()
+int main(void)
 {
-	int array[10];
-	array[0] = 5;
+	int array[5] = {0,1,2,3,4};
 	int *newArray = extendByOne(array, sizeof(array) / sizeof(array[0]));
+	for (int i = 0; i < (sizeof(array) / sizeof(array[0]))+ 1; i++)
+	{
+		printf("%d\n", newArray[i]);
+	}
 	free(newArray);
 	return 0;
 }
