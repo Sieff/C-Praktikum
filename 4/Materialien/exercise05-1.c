6da2ce9d549fcc193d70921515ef2fd0595e3910
Steffen <steff.schubert@gmx.de>
2019-11-18 15:56:25 +0100

    Aufgabe 1 angefangen

--- 4/Materialien/exercise05-1.c
+++ 4/Materialien/exercise05-1.c
@@ -1,12 +1,26 @@
+#include<math.h>
+#include<stdio.h>
+
 struct point_t {
 	double x;
 	double y;
 };
 
 double distance (struct point_t *p1, struct point_t *p2) {
-	//TODO
+	double d1 = p1->x - p2->x;
+	double d2 = p1->y - p2->y;
+	d1 = pow(d1, 2);
+	d2 = pow(d2, 2);
+	
+	double distanz = sqrt(d1+d2);
+	return distanz;
 }
 
 int main (int argc, char ** argv) {
-	//TODO
+	struct point_t p = {5, 2};
+	struct point_t q = {4, 3};
+	struct point_t * pp = &p;
+	struct point_t * pq = &q;
+	printf("%f\n", distance(pp, pq));
+	return 0;
 }
